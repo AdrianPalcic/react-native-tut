2c5cb7e3c952de95a43f6c97c33860d39d8d7974
Adrian Palcic <adrian.palcic@gmail.com>
2025-11-08 14:58:53 +0100

    Gahh damn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,30 @@
-import useTheme from "@/hooks/useTheme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+
+  const styles = createStyles(colors);
 
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Hello React Native. Pls work Cmooon</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>Toggle Theme</Text>
-      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    fontSize: 22,
-    color: "black",
-  },
-});
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.bg,
+    },
+    content: {
+      fontSize: 22,
+      color: "black",
+    },
+  });
+  return styles;
+};
