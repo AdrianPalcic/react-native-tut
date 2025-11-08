a55b527bc0b9953c68cfc089463c99e108a11e80
Adrian Palcic <adrian.palcic@gmail.com>
2025-11-08 16:12:51 +0100

    added todo

---
+++ components/TodoInput.tsx
@@ -0,0 +1,62 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation } from "convex/react";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
+import { Alert, TextInput, TouchableOpacity, View } from "react-native";
+
+const TodoInput = () => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const { colors } = useTheme();
+  const styles = createHomeStyles(colors);
+
+  const addTodo = useMutation(api.todos.addTodo);
+
+  const handleAddTodo = async () => {
+    if (newTodo.trim()) {
+      try {
+        await addTodo({ text: newTodo.trim() });
+        setNewTodo("");
+      } catch (error) {
+        Alert.alert("Neuspjeh pri dodavanju zadatka");
+      }
+    }
+  };
+
+  return (
+    <View style={styles.inputSection}>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Što trebam napraviti?"
+          value={newTodo}
+          onChangeText={setNewTodo}
+          onSubmitEditing={handleAddTodo}
+          placeholderTextColor={colors.textMuted}
+        />
+        <TouchableOpacity
+          onPress={handleAddTodo}
+          activeOpacity={0.1}
+          disabled={!newTodo.trim()}
+        >
+          <LinearGradient
+            colors={
+              newTodo.trim() ? colors.gradients.primary : colors.gradients.muted
+            }
+            style={[
+              styles.addButton,
+              !newTodo.trim() && styles.addButtonDisabled,
+            ]}
+          >
+            <Ionicons name="add-outline" size={24} color="#fff" />
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default TodoInput;
